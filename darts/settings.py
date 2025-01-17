ad5092a92d6ba48006edd8f429369a9afa2596bd
Matt Cloyd <matt.cloyd@gsa.gov>
2025-01-17 12:35:19 -0500

    Add USWDS (#37)
    This PR:
    
    - Adds USWDS
    - Adds a site title and navbar
    - Styles the search form, upload form, and documents list

--- darts/settings.py
+++ darts/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/assets/'
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+    BASE_DIR / "assets/uswds",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- search/forms.py
+++ search/forms.py
@@ -1,8 +1,25 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label=False, max_length=100)
+    query = forms.CharField(
+        label = False,
+        max_length = 140,
+        widget = forms.TextInput(attrs={
+            'class': 'usa-input',
+            'id': 'search-field-en-big',
+            'type': 'search',
+            'name': 'search',
+        })
+    )
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(label="Document title", max_length=255)
-    file = forms.FileField()
+    file = forms.FileField(
+        label = "Upload a PDF or DOCX",
+        widget = forms.TextInput(attrs={
+            'class': 'usa-file-input',
+            'id': 'file-input-single',
+            'name': 'file-input-single',
+            'type': 'file',
+        })
+    )
