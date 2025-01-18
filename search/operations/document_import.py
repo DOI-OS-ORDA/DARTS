20eed6aa76ead784fbaa2054dafa79ec1056ef51
John Skiles Skinner <john.skinner@gsa.gov>
2025-01-17 17:30:21 -0800

    Metadata arguments, function

--- search/operations/document_import.py
+++ search/operations/document_import.py
@@ -9,8 +9,10 @@ class DocumentImport:
 
     # @inject
     # def __init__(self, file_path: str, repository = Provide["repo.documents"], converter = ["ops.text"]):
-    def __init__(self, file_path: str, repository = DocumentsRepository(), converter = TextConversion):
+    def __init__(self, file_path: str, title: str, public: bool, repository = DocumentsRepository(), converter = TextConversion):
         self.file_path = file_path
+        self.title = title
+        self.public = public
         self.repository = repository
         self.text_converter = converter
 
@@ -20,6 +22,6 @@ class DocumentImport:
             filename = os.path.basename(self.file_path),
             file = open(self.file_path, "rb").read(),
             body = self.text_converter.from_filepath(self.file_path),
-            title = ''.join(random.choice(string.ascii_uppercase) for _ in range(10)),
-            public = random.choice([True, False])
+            title = self.title, #''.join(random.choice(string.ascii_uppercase) for _ in range(10)),
+            public = self.public, #self.random.choice([True, False])
         )
--- search/operations/documents_import.py
+++ search/operations/documents_import.py
@@ -1,4 +1,4 @@
-import glob
+import glob, os
 from darts.repositories import DocumentsRepository
 from search.operations.document_import import DocumentImport
 
@@ -6,14 +6,24 @@ from search.operations.document_import import DocumentImport
 # make "repo.documents" a factory
 # def __init__(self, folder_path: str = Provide["config.documents_pattern"], repository = Provide["repo.documents"]):
 class DocumentsImport:
-    def __init__(self, folder_path: str, repository = DocumentsRepository()):
-        self.folder_path = folder_path
+    def __init__(self, documents_dir: str, metadata_file: str, repository = DocumentsRepository()):
+        self.documents_dir = documents_dir
+        self.metadata_file = metadata_file
         self.repository = repository
+
 
     def call(self):
         print(f"----> [START] Starting document import...")
-        filepaths = glob.glob(self.folder_path)
+        filepaths = glob.glob(self.documents_dir)
         for path in filepaths:
             print(f"----> Importing {path}...")
-            DocumentImport(path, self.repository).call()
+            metadata = self.find_metadata()
+            DocumentImport(path, metadata['title'], metadata['public'], self.repository).call()
         print(f"----> [END] Document import complete!")
+
+
+    # Each filename appears to be prepended with a NRDARDocumentID, thus we match files to ids
+    def find_metadata(filepath):
+        #os.path.basename(filepath)
+        return({'title': 'A Fake Title', 'public': True})
+
