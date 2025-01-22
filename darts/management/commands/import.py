141808de8228641931634b46b2f681b3f902aba6
John Skiles Skinner <john.skinner@gsa.gov>
2025-01-22 13:21:15 -0800

    Import documents with metadata (#43)
    * Add metadata csv, change directory of test docs
    
    * Metadata arguments, function
    
    * Open metadata file with pandas
    
    * Get public boolean and other metadata beyond doc title
    
    * Fill unknown metadata with fake data
    
    * Remove unneeded command from README
    
    * Factor choice of metadata out into a function
    
    * Clarify comment

--- darts/management/commands/import.py
+++ darts/management/commands/import.py
@@ -6,13 +6,14 @@ class Command(BaseCommand):
     help = "Imports documents into the database, converting PDFs/Word Docs to text"
 
     def add_arguments(self, parser):
-        parser.add_argument("documents_path", nargs="?", type=str, default="darts/docs/*")
+        parser.add_argument("documents_dir", nargs="?", type=str, default="test_data/docs/*")
+        parser.add_argument("metadata_file", nargs="?", type=str, default="test_data/metadata.csv")
 
     def handle(self, *args, **options):
         self.stdout.write(
-            self.style.SUCCESS(f'Loading documents from {options["documents_path"]}')
+            self.style.SUCCESS(f'Loading documents from {options["documents_dir"]}')
         )
-        DocumentsImport(options["documents_path"]).call()
+        DocumentsImport(options["documents_dir"], options["metadata_file"]).call()
         self.stdout.write(
             self.style.SUCCESS('Successfully imported documents')
         )
--- search/operations/documents_import.py
+++ search/operations/documents_import.py
@@ -1,4 +1,4 @@
-import glob
+import glob, os, pandas, random, string
 from darts.repositories import DocumentsRepository
 from search.operations.document_import import DocumentImport
 
@@ -6,14 +6,54 @@ from search.operations.document_import import DocumentImport
 # make "repo.documents" a factory
 # def __init__(self, folder_path: str = Provide["config.documents_pattern"], repository = Provide["repo.documents"]):
 class DocumentsImport:
-    def __init__(self, folder_path: str = './darts/docs/*', repository = DocumentsRepository()):
-        self.folder_path = folder_path
+    def __init__(self, documents_dir: str, metadata_file: str, repository = DocumentsRepository()):
+        self.documents_dir = documents_dir
+        self.metadata_file = metadata_file
         self.repository = repository
+
 
     def call(self):
         print(f"----> [START] Starting document import...")
-        filepaths = glob.glob(self.folder_path)
+        filepaths = glob.glob(self.documents_dir)
+        metadata = pandas.read_csv(self.metadata_file)
         for path in filepaths:
             print(f"----> Importing {path}...")
-            DocumentImport(path, self.repository).call()
+            md = self.choose_metadata(metadata, path)
+            DocumentImport(path, md['title'], md['public'], self.repository).call()
         print(f"----> [END] Document import complete!")
+
+
+    def choose_metadata(self, metadata, path):
+        datarow = self.has_metadata(metadata, path)
+        if datarow is False: # lacking real metadata, fall back to randomly-generated
+            return(self.fake_metadata())
+        else:
+            return(self.get_metadata(datarow))
+    
+
+    def has_metadata(self, metadata, path):
+        nrdarid = os.path.basename(path).split('_')[0]
+        if not nrdarid.isdigit(): return(False) # if filename starts with NRDARDocumentID
+        datarow = metadata[metadata['NRDARDocumentID']==int(nrdarid)] # find metadata by id
+        return(False if datarow.empty else datarow)
+
+
+    def get_metadata(self, datarow):
+        return({
+            'public': datarow['PubliclyDisplayed'].iloc[0],
+            'title':  datarow['Document Name'].iloc[0],
+            'type':   datarow['Type'].iloc[0],
+            'desc':   datarow['Description'].iloc[0],
+            'date':   datarow['DocumentDt'].iloc[0],
+        })
+
+
+    def fake_metadata(self):
+        return({
+            'public': random.choice([True, False]),
+            'title':  ''.join(random.choice(string.ascii_uppercase) for _ in range(10)),
+            'type':   'Fake',
+            'desc':   'A test file with fake metadata',
+            'date':   '1/1/1970',
+        })
+
