d91c0077c3ee9e14a892654776c82dd70e9b4fd7
Matt J Cloyd <matt.cloyd@gsa.gov>
2025-01-22 15:17:25 -0500

    [WIP] working the errors out of tests

--- search/tests/operations/document_search_test.py
+++ search/tests/operations/document_search_test.py
@@ -105,10 +105,5 @@ class DocumentSearchAsSuperuserTest(TestCase):
         self.subject = DocumentSearch(
             self.search_term,
             searcher = UsersRepository.get("superuser"),
-        )
-        self.results = self.subject.call()
-        self.titles = list(map(lambda entry : entry.title, self.results))
-
-    def test_superuser_sees_all_results(self):
-        self.results = self.subject.call()
+            searcher = None, # TODO Fill in
         self.assertIn(self.known_private_title, self.titles)
